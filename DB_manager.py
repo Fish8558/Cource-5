e0792b2e3478ac62e8e40eb95a158fbe4d226435
Stas <stas_sever@mail.ru>
2024-03-11 00:50:24 +0300

    added methods for working with databases

--- DB_manager.py
+++ DB_manager.py
@@ -2,34 +2,62 @@ import psycopg2
 
 
 class DBManager:
-    """Класс для подсключения к БД"""
+    """Класс для подключения к БД"""
+
+    def __init__(self, bd_name: str, params: dict) -> None:
+        self.bd_name = bd_name
+        self.params = params
+
+    def bd_connect(self, query: str = None):
+        """Метод подключения к БД"""
+        try:
+            conn = psycopg2.connect(dbname=self.bd_name, **self.params)
+            with conn.cursor() as cur:
+                cur.execute(query)
+                result = cur.fetchall()
+                for num, data in enumerate(result, 1):
+                    print(f"{num}: {data}")
+            conn.close()
+        except Exception:
+            print(f"Ошибка подключения к базе данных. Проверьте корректность данных.")
 
     def get_companies_and_vacancies_count(self):
         """Метод получает список всех компаний и
-        количество вакансий у каждой компани"""
+        количество вакансий у каждой компании"""
+        sql_query = ("""SELECT employers.company_name, COUNT(*) AS count_vacancies
+                        FROM vacancies
+                        JOIN employers USING (employer_id)
+                        GROUP BY employers.company_name
+                        ORDER BY count_vacancies DESC""")
+        self.bd_connect(sql_query)
 
-        with psycopg2.connect(host="localhost", database="course_work_5",
-                              user="postgres", password="12345") as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT company_name, COUNT(vacancies_name) AS count_vacancies  "
-                            f"FROM employers "
-                            f"JOIN vacancies USING (employer_id) "
-                            f"GROUP BY employers.company_name")
-                result = cur.fetchall()
-            conn.commit()
-        return result
-
-    @property
     def get_all_vacancies(self, keyword=None):
-        """Метод получает среднюю по ЗП вакансию
-        у которыз ЗП выше средней по всемвакансия"""
-        with psycopg2.connect(host="localhost", database="course_work_5",
-                              user="postgres", password="12345") as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM vacancies "
-                            f"WHERE lower(vacancies_name) LIKE '%{keyword}%' "
-                            f"OR lower(vacancies_name) LIKE '%{keyword}'"
-                            f"OR lower(vacancies_name) LIKE '{keyword}%';")
-                result = cur.fetchall()
-            conn.commit()
-        return result
+        """Метод получает список всех вакансий: название компании,
+        название вакансии, зарплата и ссылка на вакансию.
+        """
+        sql_query = ("""SELECT employers.company_name, vacancies_name, payment, vacancies_url 
+                        FROM vacancies
+                        JOIN employers USING(employer_id)""")
+        self.bd_connect(sql_query)
+
+    def get_avg_salary(self):
+        """Метод получает среднюю зарплату по вакансиям."""
+        sql_query = ("""SELECT AVG(payment) AS avg_salary 
+                        FROM vacancies""")
+        self.bd_connect(sql_query)
+
+    def get_vacancies_with_higher_salary(self):
+        """Метод получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        sql_query = ("""SELECT * FROM vacancies
+                        WHERE payment > (SELECT AVG(payment) FROM vacancies)
+                        ORDER BY payment DESC""")
+        self.bd_connect(sql_query)
+
+    def get_vacancies_with_keyword(self, keyword: str) -> None:
+        """Метод получает список всех вакансий,
+        в названии которых содержатся переданные в метод слова, например python."""
+        sql_query = (f"""SELECT * FROM vacancies
+                         WHERE lower(vacancies_name) LIKE '%{keyword}%'
+                         OR lower(vacancies_name) LIKE '%{keyword}'
+                         OR lower(vacancies_name) LIKE '{keyword}%'""")
+        self.bd_connect(sql_query)
